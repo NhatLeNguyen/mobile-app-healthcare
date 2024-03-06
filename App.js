8f59257e9479cf7b2682746b5e8378e7cde5a9a4
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-03-06 23:51:14 +0700

    nothin

--- App.js
+++ App.js
@@ -1,66 +1,107 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet , Text} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons, FontAwesome } from '@expo/vector-icons'
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
 // Screens
-import HomeScreen from "./src/screens/HomeScreen";
-
+import HomeScreen from "./src/screens/Home/HomeScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
-          color = focused 
-              ? '#6495ED'
-              : 'gray'
-          if(route.name === 'Home'){
-            iconName = focused
-              ? 'information'
-              : 'information'
-            return <FontAwesome name="home" size={size} color={color} />
-          } else if (route.name === "Empty"){
-            return <FontAwesome name='star-half-empty' size={size} color={color} />
-          } else if (route.name === "Empty1"){
-            return <FontAwesome name='star-half-empty' size={size} color={color} />
-          } else if(route.name === 'User'){
-            return <FontAwesome name='user' size={size} color={color} />
-          }
-        },
-        tabBarLabel: ({ focused, color }) => { // Add tabBarLabel
-          let label;
-          if (route.name === 'Home') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Màn hình chính</Text>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            color = focused ? "#6495ED" : "gray";
+            if (route.name === "Home") {
+              iconName = focused ? "information" : "information";
+              return <FontAwesome name="home" size={size} color={color} />;
+            } else if (route.name === "Empty") {
+              return (
+                <FontAwesome name="star-half-empty" size={size} color={color} />
+              );
+            } else if (route.name === "Empty1") {
+              return (
+                <FontAwesome name="star-half-empty" size={size} color={color} />
+              );
+            } else if (route.name === "User") {
+              return <FontAwesome name="user" size={size} color={color} />;
             }
-          } else if (route.name === 'Empty') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Empty</Text>;
+          },
+          tabBarLabel: ({ focused, color }) => {
+            // Add tabBarLabel
+            let label;
+            if (route.name === "Home") {
+              if (focused) {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "#ED64B4" : "gray",
+                      fontSize: 12,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Màn hình chính
+                  </Text>
+                );
+              }
+            } else if (route.name === "Empty") {
+              if (focused) {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "#ED8F64" : "gray",
+                      fontSize: 12,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Empty
+                  </Text>
+                );
+              }
+            } else if (route.name === "Empty1") {
+              if (focused) {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "#6495ED" : "gray",
+                      fontSize: 12,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Empty1
+                  </Text>
+                );
+              }
+            } else if (route.name === "User") {
+              if (focused) {
+                return (
+                  <Text
+                    style={{
+                      color: focused ? "#6495ED" : "gray",
+                      fontSize: 12,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Hồ sơ
+                  </Text>
+                );
+              }
             }
-          } else if (route.name === 'Empty1') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Empty1</Text>;
-            }
-          } else if (route.name === 'User') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Hồ sơ</Text>;
-            }
-          }
-          return null
-        },
-        tabBarStyle: {
-          backgroundColor: '#F8F8F8', // Màu nền của tabBar
-        }
-      })}
+            return null;
+          },
+          tabBarStyle: {
+            backgroundColor: "#F8F8F8", // Màu nền của tabBar
+          },
+        })}
       >
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Empty' component={HomeScreen} />
-        <Tab.Screen name='Empty1' component={HomeScreen} />
-        <Tab.Screen name='User' component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Empty" component={HomeScreen} />
+        <Tab.Screen name="Empty1" component={HomeScreen} />
+        <Tab.Screen name="User" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
