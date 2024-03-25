2c64c91642a00e73c50c77df23badeb4d7b0ce3c
KhangSimple <you@example.com>
2024-03-25 09:34:42 +0700

    complete create database and add data ( easy query)

--- App.js
+++ App.js
@@ -1,106 +1,177 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, StyleSheet , Text} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons, FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons'
+import {
+  Ionicons,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 // Screens
 import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
 import HomeNavigator from "./src/screens/HomeScreen/HomeNavigator";
 import StartPracticeScreenNavigator from "./src/screens/StartPracticeScreen/StartPracticeScreenNavigator";
-import SQLite from 'react-native-sqlite-storage'
-import * as SplashScreen from 'expo-splash-screen'
+import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
 import { Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
+import * as FileSystem from "expo-file-system";
+import * as SQLite from "expo-sqlite";
+import { Asset } from "expo-asset";
 
-// const db = SQLite.openDatabase('health-care.db')
+const db = SQLite.openDatabase("health-care.db");
+db.transaction(tx => {
+  tx.executeSql(
+    "create table if not exists practicehistory (practice_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id TEXT, start_time TEXT, end_time TEXT, date TEXT, steps INT, distances REAL, practice_time TEXT, caloris REAL, posList TEXT)"
+  );
+});
+
 
 const Tab = createBottomTabNavigator();
-SplashScreen.preventAutoHideAsync()
-  .then((result) => console.log(`SplashScreen preventAutoHideAsync Success ${result}`));
+SplashScreen.preventAutoHideAsync().then((result) =>
+  console.log(`SplashScreen preventAutoHideAsync Success ${result}`)
+);
 
 export default function App() {
   const [isAppReady, setIsAppReady] = useState(false);
 
   const [fontsLoaded, fontError] = useFonts({
     Inter_500Medium,
-    Inter_600SemiBold
-  })
-  const onLayoutRoot = useCallback(async() => {
-    if(fontsLoaded){
+    Inter_600SemiBold,
+  });
+  const onLayoutRoot = useCallback(async () => {
+    if (fontsLoaded) {
       console.log("Load font successfully");
-      await SplashScreen.hideAsync()
-      setIsAppReady(true)
+      await SplashScreen.hideAsync();
+      setIsAppReady(true);
     }
-  }, [fontsLoaded])
+  }, [fontsLoaded]);
   useEffect(() => {
-    async function onLayout(){
-      if(fontsLoaded){
+    async function onLayout() {
+      if (fontsLoaded) {
         setTimeout(() => {
-          SplashScreen.hideAsync()
-          setIsAppReady(true)
-        }, 5000)
+          SplashScreen.hideAsync();
+          setIsAppReady(true);
+        }, 5000);
       }
     }
-    onLayout()
-  }, [fontsLoaded])
+    onLayout();
+  }, [fontsLoaded]);
   console.log(isAppReady);
-  if(!isAppReady){
+  if (!isAppReady) {
     return null;
   }
   return (
     <View style={styles.container} onLayout={onLayoutRoot}>
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName;
-          color = focused 
-              ? '#6495ED'
-              : 'gray'
-          if(route.name === 'Home'){
-            iconName = focused
-              ? 'information'
-              : 'information'
-            return <FontAwesome name="home" size={size} color={color} />
-          } else if (route.name === "Practice"){
-            return <MaterialCommunityIcons name="electron-framework" size={size} color={color} />
-          } else if (route.name === "Empty1"){
-            return <FontAwesome name='star-half-empty' size={size} color={color} />
-          } else if(route.name === 'User'){
-            return <FontAwesome name='user' size={size} color={color} />
-          }
-        },
-        tabBarLabel: ({ focused, color }) => { // Add tabBarLabel
-          let label;
-          if (route.name === 'Home') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Màn hình chính</Text>
-            }
-          } else if (route.name === 'Practice') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Practice</Text>;
-            }
-          } else if (route.name === 'Empty1') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Practice1</Text>;
-            }
-          } else if (route.name === 'User') {
-            if(focused){
-              return <Text style={{ color: focused ? '#6495ED' : 'gray', fontSize: 12, fontWeight:'bold'}}>Hồ sơ</Text>;
-            }
-          }
-          return null
-        },
-        tabBarStyle: {
-          backgroundColor: '#F8F8F8', // Màu nền của tabBar
-        }
-      })}
-      >
-        <Tab.Screen name='Home' component={HomeNavigator} options={{ headerShown: false}}/>
-        <Tab.Screen name='Practice' component={StartPracticeScreenNavigator} options={{ headerShown: false }}/>
-        <Tab.Screen name='Empty1' component={HomeScreen} />
-        <Tab.Screen name='User' component={HomeScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              color = focused ? "#6495ED" : "gray";
+              if (route.name === "Home") {
+                iconName = focused ? "information" : "information";
+                return <FontAwesome name="home" size={size} color={color} />;
+              } else if (route.name === "Practice") {
+                return (
+                  <MaterialCommunityIcons
+                    name="electron-framework"
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Empty1") {
+                return (
+                  <FontAwesome
+                    name="star-half-empty"
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "User") {
+                return <FontAwesome name="user" size={size} color={color} />;
+              }
+            },
+            tabBarLabel: ({ focused, color }) => {
+              // Add tabBarLabel
+              let label;
+              if (route.name === "Home") {
+                if (focused) {
+                  return (
+                    <Text
+                      style={{
+                        color: focused ? "#6495ED" : "gray",
+                        fontSize: 12,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Màn hình chính
+                    </Text>
+                  );
+                }
+              } else if (route.name === "Practice") {
+                if (focused) {
+                  return (
+                    <Text
+                      style={{
+                        color: focused ? "#6495ED" : "gray",
+                        fontSize: 12,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Practice
+                    </Text>
+                  );
+                }
+              } else if (route.name === "Empty1") {
+                if (focused) {
+                  return (
+                    <Text
+                      style={{
+                        color: focused ? "#6495ED" : "gray",
+                        fontSize: 12,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Practice1
+                    </Text>
+                  );
+                }
+              } else if (route.name === "User") {
+                if (focused) {
+                  return (
+                    <Text
+                      style={{
+                        color: focused ? "#6495ED" : "gray",
+                        fontSize: 12,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Hồ sơ
+                    </Text>
+                  );
+                }
+              }
+              return null;
+            },
+            tabBarStyle: {
+              backgroundColor: "#F8F8F8", // Màu nền của tabBar
+            },
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeNavigator}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Practice"
+            component={StartPracticeScreenNavigator}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen name="Empty1" component={HomeScreen} />
+          <Tab.Screen name="User" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
--- src/screens/HomeScreen/HomeCircleInfo.js
+++ src/screens/HomeScreen/HomeCircleInfo.js
@@ -1,148 +1,184 @@
 import React, { Component, useState } from "react";
-import { View, Text, StyleSheet, Pressable, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import CircleWithPercentage from "../../components/CircleWithPercentage";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import { Inter_500Medium } from "@expo-google-fonts/inter";
 import { useFonts } from "expo-font";
 import Fonts from "../../constants/Fonts";
 import { useNavigation } from "@react-navigation/native";
-
+import * as SQLite from "expo-sqlite/next";
+const db = SQLite.openDatabaseAsync("health-care.db");
 
 function HomeCircleInfo() {
-  // const [fontsLoaded] = useFonts({
-  //   Inter_400Regular: Fonts.Inter_Regular,
-  //   Inter_Medium: Fonts.Inter_Medium,
-  //   Inter_700Bold: Fonts.Inter_Bold,
-  // });
-  // if (!fontsLoaded) {
-  //   console.log("Loading...");
-  // }
-  // const [fontsLoaded] = useFonts({
-  //   Inter_500Medium
-  // })
-  const [isPressedHeart , setIsPressHeart] = useState(false)
-  const [isPressedStep , setIsPressStep] = useState(false)
-  const [isPressedCalo , setIsPressCalo] = useState(false)
-  const [isPressedDistance , setIsPressDistance] = useState(false)
-  const [isPressedTime , setIsPressTime] = useState(false)
+  const [isPressedHeart, setIsPressHeart] = useState(false);
+  const [isPressedStep, setIsPressStep] = useState(false);
+  const [isPressedCalo, setIsPressCalo] = useState(false);
+  const [isPressedDistance, setIsPressDistance] = useState(false);
+  const [isPressedTime, setIsPressTime] = useState(false);
+  const useDatabase =async () => {
+    // console.log("Inserting ...");
+    // (await db).runSync('insert into practicehistory(user_id, start_time, end_time, date, steps, distances, practice_time, caloris, posList) values(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    // ['1', '00:01:23', '00:01:23', '2024-03-19', '50', '0', '1:49', '1', '[{\"latitude\":20.9336862,\"longitude\":105.6472441,\"latitudeDelta\":0.01,\"longitudeDelta\":0.01}]'])
+    // console.log("Inserting success!!");
+  };
+  const checkDB =async () => {
+    console.log("Checking ...");
+    const allRows = (await db).getAllSync('select * from practicehistory');
+    for (const row of allRows){
+      console.log(row);
+    }
+    console.log(allRows.length);
+  };
+  const deleteRow = async () => {
+    // console.log("Delete row ...");
+    // (await db).runSync('delete from practicehistory');
+  }
 
-  const navigation = useNavigation()
-    return ( 
-        <View style={styles.circleContainer}>
-          <CircleWithPercentage
-            diameter={220}
-            color="#00c3a6"
-            restColor="#c8f2ec"
-            value={50}
-            MAX_VALUE={1000}
-            paddingCircle={0}
-            paddingText={60}
-            textSize={45}
-          />
-          <CircleWithPercentage
-            diameter={180}
-            color="#184ea6"
-            restColor="#dfe7f3"
-            value={500}
-            MAX_VALUE={1000}
-            paddingCircle={20}
-            paddingText={90}
-            textSize={30}
-          />
-          <View style={{ paddingTop: 220, flexDirection: "row" }}>
-            <Pressable
-              style={[{
-                flexDirection: "row",
-                alignItems: "center",
-                padding: 15,
-              }, isPressedHeart && styles.pressedButton]}
-              onPress={() => console.log("Hehe")}
-              onPressIn={() => setIsPressHeart(true)}
-              onPressOut={() => setIsPressHeart(false)}
-            >
-              <FontAwesome name="heartbeat" size={16} color={"green"} />
-              <Text style={{ marginLeft: 5, fontSize: 16, fontFamily:'Inter_500Medium'}}>Điểm nhịp tim</Text>
-            </Pressable>
+  const navigation = useNavigation();
+  return (
+    <View style={styles.circleContainer}>
+      <CircleWithPercentage
+        diameter={220}
+        color="#00c3a6"
+        restColor="#c8f2ec"
+        value={50}
+        MAX_VALUE={1000}
+        paddingCircle={0}
+        paddingText={60}
+        textSize={45}
+      />
+      <CircleWithPercentage
+        diameter={180}
+        color="#184ea6"
+        restColor="#dfe7f3"
+        value={500}
+        MAX_VALUE={1000}
+        paddingCircle={20}
+        paddingText={90}
+        textSize={30}
+      />
+      <View style={{ paddingTop: 220, flexDirection: "row" }}>
+        <Pressable
+          style={[
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              padding: 15,
+            },
+            isPressedHeart && styles.pressedButton,
+          ]}
+          onPress={() => console.log("Hehe")}
+          onPressIn={() => setIsPressHeart(true)}
+          onPressOut={() => setIsPressHeart(false)}
+        >
+          <FontAwesome name="heartbeat" size={16} color={"green"} />
+          <Text
+            style={{
+              marginLeft: 5,
+              fontSize: 16,
+              fontFamily: "Inter_500Medium",
+            }}
+          >
+            Điểm nhịp tim
+          </Text>
+        </Pressable>
 
-            <Pressable
-              style={[{
-                flexDirection: "row",
-                alignItems: "center",
-                marginLeft: 6,
-                padding: 15,
-              }, isPressedStep && styles.pressedButton]}
-              onPress={() => navigation.navigate('ActivityDetail')}
-              onPressIn={() => setIsPressStep(true)}
-              onPressOut={() => setIsPressStep(false)}
-            >
-              <Ionicons name="footsteps" size={16} color={"blue"} />
-              <Text style={{ marginLeft: 5, fontSize: 16, fontFamily:'Inter_500Medium'}}>Bước</Text>
-            </Pressable>
-          </View>
+        <Pressable
+          style={[
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              marginLeft: 6,
+              padding: 15,
+            },
+            isPressedStep && styles.pressedButton,
+          ]}
+          onPress={() => navigation.navigate("ActivityDetail")}
+          onPressIn={() => setIsPressStep(true)}
+          onPressOut={() => setIsPressStep(false)}
+        >
+          <Ionicons name="footsteps" size={16} color={"blue"} />
+          <Text
+            style={{
+              marginLeft: 5,
+              fontSize: 16,
+              fontFamily: "Inter_500Medium",
+            }}
+          >
+            Bước
+          </Text>
+        </Pressable>
+      </View>
 
-          <View style={{flexDirection: "row" }}>
-            <Pressable
-              style={[styles.pressAble,isPressedCalo && styles.pressedButton]}
-              onPress={() => console.log("Hehe")}
-              onPressIn={() => setIsPressCalo(true)}
-              onPressOut={() => setIsPressCalo(false)}
-            >
-              <Text style={styles.number}>{1226/1000}</Text>
-              <Text style={styles.subText}>Calo</Text>
-            </Pressable>
+      <View style={{ flexDirection: "row" }}>
+        <Pressable
+          style={[styles.pressAble, isPressedCalo && styles.pressedButton]}
+          onPress={() => useDatabase()}
+          onPressIn={() => setIsPressCalo(true)}
+          onPressOut={() => setIsPressCalo(false)}
+        >
+          <Text style={styles.number}>{1226 / 1000}</Text>
+          <Text style={styles.subText}>Calo</Text>
+        </Pressable>
 
-            <Pressable
-              style={[styles.pressAble,isPressedDistance && styles.pressedButton]}
-              onPress={() => console.log("Hehe")}
-              onPressIn={() => setIsPressDistance(true)}
-              onPressOut={() => setIsPressDistance(false)}
-            >
-              <Text style={styles.number}>{0.68}</Text>
-              <Text style={styles.subText}>Km</Text>
-            </Pressable>
-            
-            <Pressable
-              style={[styles.pressAble, isPressedTime && styles.pressedButton]}
-              onPress={() => console.log("Hehe")}
-              onPressIn={() => setIsPressTime(true)}
-              onPressOut={() => setIsPressTime(false)}
-            >
-              <Text style={styles.number}>23</Text>
-              <Text style={styles.subText}>Phút vận động</Text>
-            </Pressable>
-          </View>
-        </View>
-    );
+        <Pressable
+          style={[styles.pressAble, isPressedDistance && styles.pressedButton]}
+          onPress={() => checkDB()}
+          onPressIn={() => setIsPressDistance(true)}
+          onPressOut={() => setIsPressDistance(false)}
+        >
+          <Text style={styles.number}>{0.68}</Text>
+          <Text style={styles.subText}>Km</Text>
+        </Pressable>
+
+        <Pressable
+          style={[styles.pressAble, isPressedTime && styles.pressedButton]}
+          onPress={() => deleteRow()}
+          onPressIn={() => setIsPressTime(true)}
+          onPressOut={() => setIsPressTime(false)}
+        >
+          <Text style={styles.number}>23</Text>
+          <Text style={styles.subText}>Phút vận động</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {},
-    circleContainer: {
-      marginTop: 60,
-      alignItems: "center",
-    },
-    text: {
-      fontSize: 16,
-      color: "#D400FF",
-    },
-    number: {
-      fontSize: 18,
-      color: '#1a9be8',
-      fontWeight: 'bold'
-    },
-    pressAble: {
-      alignItems: "center",
-      padding: 15,
-      // backgroundColor: 'red',
-      width: 105
-    },
-    subText: {
-      fontSize: 16,
-      color: '#3c4043',
-      textAlign: 'center'
-    },
-    pressedButton: {
-      backgroundColor: '#ECECEC'
-    }
-  });
+  container: {},
+  circleContainer: {
+    marginTop: 60,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 16,
+    color: "#D400FF",
+  },
+  number: {
+    fontSize: 18,
+    color: "#1a9be8",
+    fontWeight: "bold",
+  },
+  pressAble: {
+    alignItems: "center",
+    padding: 15,
+    // backgroundColor: 'red',
+    width: 105,
+  },
+  subText: {
+    fontSize: 16,
+    color: "#3c4043",
+    textAlign: "center",
+  },
+  pressedButton: {
+    backgroundColor: "#ECECEC",
+  },
+});
 export default HomeCircleInfo;
